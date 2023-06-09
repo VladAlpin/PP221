966057b2fe76802e3d6c8b206ee02d7d22fa6ef3
VladAlpin <zxczxczxc33513@gmail.com>
2023-06-09 16:22:27 +1000

    pp221

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -16,20 +16,20 @@ public class MainApp {
 
         UserService userService = context.getBean(UserService.class);
 
-//        User user1 = new User("name1", "lastName1", "1@gamil.com");
-//        User user2 = new User("name2", "lastName2", "2@gamil.com");
-//        User user3 = new User("name3", "lastName3", "3@gamil.com");
-//        User user4 = new User("name4", "lastName4", "4@gamil.com");
-//
-//        Car car1 = new Car("model", 1);
-//        Car car2 = new Car("model", 2);
-//        Car car3 = new Car("model", 3);
-//        Car car4 = new Car("model", 4);
+        User user1 = new User("name1", "lastName1", "1@gamil.com");
+        User user2 = new User("name2", "lastName2", "2@gamil.com");
+        User user3 = new User("name3", "lastName3", "3@gamil.com");
+        User user4 = new User("name4", "lastName4", "4@gamil.com");
 
-//        userService.add(user1.setCar(car1).setUser(user1));
-//        userService.add(user2.setCar(car2).setUser(user2));
-//        userService.add(user3.setCar(car3).setUser(user3));
-//        userService.add(user4.setCar(car4).setUser(user4));
+        Car car1 = new Car("model1", 1);
+        Car car2 = new Car("model2", 2);
+        Car car3 = new Car("model3", 3);
+        Car car4 = new Car("model4", 4);
+
+        userService.add(user1.setUserByCar(car1).setUser(user1));
+        userService.add(user2.setUserByCar(car2).setUser(user2));
+        userService.add(user3.setUserByCar(car3).setUser(user3));
+        userService.add(user4.setUserByCar(car4).setUser(user4));
 
         List<User> users = userService.listUsers();
         for (User user : users) {
